010b96ed5db28e4ff19410d969ffec001b51a757
lyj416487516 <416487516@qq.com>
2022-06-09 17:34:33 +0800

    后台商品

--- src/main/java/org/example/admin/controller/CommodityController.java
+++ src/main/java/org/example/admin/controller/CommodityController.java
@@ -1,7 +1,26 @@
 package org.example.admin.controller;
 
+import org.example.admin.service.CommodityService;
+import org.example.shopping.bean.Shops;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("commodity")
 public class CommodityController {
 
+    @Autowired
+    private CommodityService commodityService;
 
+    @RequestMapping("index")
+    public String index() {
+        List<Shops> shopsList = commodityService.findAll();
+        System.out.println(shopsList);
+
+        return "/admin/commodity/index";
+    }
 
 }
--- src/main/java/org/example/admin/service/CommodityService.java
+++ src/main/java/org/example/admin/service/CommodityService.java
@@ -1,4 +1,11 @@
 package org.example.admin.service;
 
+import org.example.shopping.bean.Shops;
+
+import java.util.List;
+
 public interface CommodityService {
+
+    List<Shops> findAll();
+
 }
